bd1170445b44d93067d02d7c3df034e13a846fc7
Francesco Bianchi <f.bianchi@palmabit.com>
2018-08-06 14:46:13 +0200

    add optional close db

--- src/crud.js
+++ src/crud.js
@@ -2,32 +2,43 @@ const co = require('co')
 const logger = require('./logger')
 const connectToDatabase = require('./db')
 
+function closeDB(db, closeDB) {
+  if (closeDB && db) {
+    db.close();
+  }
+}
 class Crud {
 
-  constructor(stringConnection, collectionName) {
+  constructor(stringConnection, collectionName, closeDB) {
     this.STRING_CONNECTION = stringConnection
     this.COLLECTION_NAME = collectionName
+    this.CLOSE_DB = closeDB
   }
 
   list({ query = {}, sort = {}, skip = 0, limit = 10 }, callback) {
 
     const STRING_CONNECTION = this.STRING_CONNECTION
     const COLLECTION_NAME = this.COLLECTION_NAME
+    const CLOSE_DB = this.CLOSE_DB
+
+    let db;
 
     co(function* () {
 
-      const db = yield connectToDatabase(STRING_CONNECTION)
+      db = yield connectToDatabase(STRING_CONNECTION)
       const col = db.collection(COLLECTION_NAME)
 
       logger.info('Successfully connect to: ', STRING_CONNECTION)
       const docs = yield col.find(query).sort(sort).skip(skip).limit(limit).toArray()
 
+      closeDB(db, CLOSE_DB);
+
       callback(null, docs)
 
     }).catch((err) => {
       logger.error(err)
+      closeDB(db, CLOSE_DB);
       callback(err)
-
     })
   }
 
@@ -35,19 +46,25 @@ class Crud {
 
     const STRING_CONNECTION = this.STRING_CONNECTION
     const COLLECTION_NAME = this.COLLECTION_NAME
+    const CLOSE_DB = this.CLOSE_DB
+
+    let db;
 
     co(function* () {
 
-      const db = yield connectToDatabase(STRING_CONNECTION)
+      db = yield connectToDatabase(STRING_CONNECTION)
       const col = db.collection(COLLECTION_NAME)
 
       logger.info('Successfully connect to: ', STRING_CONNECTION)
       const docs = yield col.aggregate(stages).sort(sort).skip(skip).limit(limit).toArray()
 
+      closeDB(db, CLOSE_DB);
+
       callback(null, docs)
 
     }).catch((err) => {
       logger.error(err)
+      closeDB(db, CLOSE_DB);
       callback(err)
 
     })
@@ -57,22 +74,27 @@ class Crud {
 
     const STRING_CONNECTION = this.STRING_CONNECTION
     const COLLECTION_NAME = this.COLLECTION_NAME
+    const CLOSE_DB = this.CLOSE_DB
+
+    let db;
 
     co(function* () {
 
-      const db = yield connectToDatabase(STRING_CONNECTION)
+      db = yield connectToDatabase(STRING_CONNECTION)
       const col = db.collection(COLLECTION_NAME)
 
       logger.info('Successfully connect to: ', STRING_CONNECTION)
       doc.createdAt = new Date()
       const res = yield col.insertOne(doc)
 
+      closeDB(db, CLOSE_DB);
+
       callback(null, res.ops[0])
 
     }).catch((err) => {
       logger.error(err)
+      closeDB(db, CLOSE_DB);
       callback(err)
-
     })
   }
 
@@ -80,10 +102,13 @@ class Crud {
 
     const STRING_CONNECTION = this.STRING_CONNECTION
     const COLLECTION_NAME = this.COLLECTION_NAME
+    const CLOSE_DB = this.CLOSE_DB
+
+    let db;
 
     co(function* () {
 
-      const db = yield connectToDatabase(STRING_CONNECTION)
+      db = yield connectToDatabase(STRING_CONNECTION)
       const col = db.collection(COLLECTION_NAME)
 
       logger.info('Successfully connect to: ', STRING_CONNECTION)
@@ -96,10 +121,14 @@ class Crud {
         }, {
           returnOriginal: false
         })
+
+      closeDB(db, CLOSE_DB);
+
       callback(null, res.value)
 
     }).catch((err) => {
       logger.error(err)
+      closeDB(db, CLOSE_DB);
       callback(err)
 
     })
@@ -109,10 +138,13 @@ class Crud {
 
     const STRING_CONNECTION = this.STRING_CONNECTION
     const COLLECTION_NAME = this.COLLECTION_NAME
+    const CLOSE_DB = this.CLOSE_DB
+
+    let db;
 
     co(function* () {
 
-      const db = yield connectToDatabase(STRING_CONNECTION)
+      db = yield connectToDatabase(STRING_CONNECTION)
       const col = db.collection(COLLECTION_NAME)
 
       logger.info('Successfully connect to: ', STRING_CONNECTION)
@@ -120,12 +152,14 @@ class Crud {
         _id: id
       })
 
+      closeDB(db, CLOSE_DB);
+
       callback(null, res)
 
     }).catch((err) => {
       logger.error(err)
+      closeDB(db, CLOSE_DB);
       callback(err)
-
     })
   }
 
@@ -133,20 +167,26 @@ class Crud {
 
     const STRING_CONNECTION = this.STRING_CONNECTION
     const COLLECTION_NAME = this.COLLECTION_NAME
+    const CLOSE_DB = this.CLOSE_DB
+
+    let db;
 
     co(function* () {
 
-      const db = yield connectToDatabase(STRING_CONNECTION)
+      db = yield connectToDatabase(STRING_CONNECTION)
       const col = db.collection(COLLECTION_NAME)
 
       logger.info('Successfully connect to: ', STRING_CONNECTION)
       const res = yield col.findOne(query)
+
+      closeDB(db, CLOSE_DB);
+
       callback(null, res)
 
     }).catch((err) => {
       logger.error(err)
+      closeDB(db, CLOSE_DB);
       callback(err)
-
     })
   }
 
@@ -154,18 +194,25 @@ class Crud {
 
     const STRING_CONNECTION = this.STRING_CONNECTION
     const COLLECTION_NAME = this.COLLECTION_NAME
+    const CLOSE_DB = this.CLOSE_DB
+
+    let db;
 
     co(function* () {
 
-      const db = yield connectToDatabase(STRING_CONNECTION)
+      db = yield connectToDatabase(STRING_CONNECTION)
       const col = db.collection(COLLECTION_NAME)
 
       logger.info('Successfully connect to: ', STRING_CONNECTION)
       const count = yield col.count(query)
+
+      closeDB(db, CLOSE_DB);
+
       callback(null, count)
 
     }).catch((err) => {
       logger.error(err)
+      closeDB(db, CLOSE_DB);
       callback(err)
 
     })
